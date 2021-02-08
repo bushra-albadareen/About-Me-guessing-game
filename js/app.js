be386d9c7913937a25799595248cdb48abcc2868
bushra <bushra.albadareen@gmail.com>
2021-02-08 21:15:40 +0200

    new

---
+++ js/app.js
@@ -0,0 +1,66 @@
+'use strict';
+let username = prompt("Please enter your Name");
+
+let bornPlace = prompt('was I born in amman?(yes/no)').toUpperCase();
+
+if (bornPlace === 'no'){
+
+            alert('right answer, welcome' + username );
+}else{
+
+            alert('wrong answer, welcome' + username );
+}
+                
+
+
+
+let majorFiled = prompt('is nutrition my major?(yes/no)').toUpperCase();
+
+if (majorFiled === 'yes'){
+
+            alert('right answer, welcome' + username );
+}else{
+
+            alert('wrong answer, welcome' + username );
+}
+        
+
+        
+let workPlace = prompt('was I working on hospital?(yes/no)').toUpperCase();
+
+if (workPlace === 'yes'){
+
+            alert('right answer, welcome' + username );
+
+}else{
+            alert('wrong answer, welcome' + username );
+        
+}
+        
+
+let experience = prompt('have I an experience in teaching?(yes/no)').toUpperCase();
+
+if (experience === 'yes'){
+
+            alert('right answer, welcome' + username );
+       
+        
+}else{
+            alert('wrong answer, welcome' + username );
+        
+}
+        
+let graduated = prompt('am I freshly graduated?(yes/no)').toUpperCase();
+        
+if (graduated === 'no'){
+
+            alert('right answer, welcome' + username );
+        
+}else{
+            alert('wrong answer, welcome' + username );
+
+        }
+        
+        
+ 
+document.write(alert);
