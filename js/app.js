27d7905c4b3f0d77731427d2c4beed68ebc8bedd
bushra <bushra.albadareen@gmail.com>
2021-02-08 23:17:23 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,48 +1,48 @@
 'use strict';
-let username = prompt("Please enter your Name");
+let userName = prompt("Please enter your Name");
 
-let bornPlace = prompt('was I born in amman?(yes/no)').toUpperCase();
+let bornPlace = prompt('was I born in amman?(yes/no)').toLowerCase();
 
 if (bornPlace === 'no'){
 
-            alert('right answer, welcome' + username );
+            alert('right answer, welcome' + userName );
 }else{
 
-            alert('wrong answer, welcome' + username );
+            alert('wrong answer, welcome' + userName );
 }
                 
 
 
 
-let majorFiled = prompt('is nutrition my major?(yes/no)').toUpperCase();
+let majorFiled = prompt('is nutrition my major?(yes/no)').toLowerCase();
 
 if (majorFiled === 'yes'){
 
-            alert('right answer, welcome' + username );
+            alert('right answer, welcome' + userName );
 }else{
 
-            alert('wrong answer, welcome' + username );
+            alert('wrong answer, welcome' + userName );
 }
         
 
         
-let workPlace = prompt('was I working on hospital?(yes/no)').toUpperCase();
+let workPlace = prompt('was I working on hospital?(yes/no)').toLowerCase();
 
 if (workPlace === 'yes'){
 
-            alert('right answer, welcome' + username );
+            alert('right answer, welcome' + userName );
 
 }else{
-            alert('wrong answer, welcome' + username );
+            alert('wrong answer, welcome' + userName );
         
 }
         
 
-let experience = prompt('have I an experience in teaching?(yes/no)').toUpperCase();
+let experience = prompt('have I an experience in teaching?(yes/no)').toLowerCase();
 
 if (experience === 'yes'){
 
-            alert('right answer, welcome' + username );
+            alert('right answer, welcome' + userName );
        
         
 }else{
@@ -50,14 +50,14 @@ if (experience === 'yes'){
         
 }
         
-let graduated = prompt('am I freshly graduated?(yes/no)').toUpperCase();
+let graduated = prompt('am I freshly graduated?(yes/no)').toLowerCase();
         
 if (graduated === 'no'){
 
-            alert('right answer, welcome' + username );
+            alert('right answer, welcome' + userName );
         
 }else{
-            alert('wrong answer, welcome' + username );
+            alert('wrong answer, welcome' + userName );
 
         }
         
