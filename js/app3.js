9372a1f92957f4a6eb5425682e661d3bab396e74
bushra <bushra.albadareen@gmail.com>
2021-02-09 22:29:17 +0200

    new

---
+++ js/app3.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+let placeVisit = ['Riyadh', 'Qatar', 'America', 'China', 'Amman', 'Jordan'];
+let chance= 6;
+let userInput1 = prompt ('try to guess a place that i visit');
+
+
+for (let i=0; i < chance; i++){
+
+  if (userInput1 === bornPlace[i] ){
+    alert ('right answer');
+  } else  {
+    alert ('wrong answer, try again');
+    userInput1 = prompt ('try to guess a place that i visit');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer ....' + placeVisit);
+  break;
+}
+
+
+
+
+
+
+
+
+
+let jobFiled = ['Nurse', 'teacher', 'athletic', 'engineer', 'nutritionist', 'doctor'];
+let eyeColor = ['Black', 'hazel', 'brown', 'blue', 'green', 'gray'];
+let pet = ['cat', 'puppy', 'dog',​'​hamster', 'bird', 'squirrel'];
+let juice = ['Apple', 'orange', 'strawberry', 'lemon', 'pineapple', 'guava'];
+let movieType = ['Romantic', 'horror', 'action', 'sci-fi', 'animation', 'documentary'];
+let color = ['Red', 'Green', 'Black', 'Blue', 'Pink', 'Indigo'];
