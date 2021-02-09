700d2bf5a02aab63bed03b820edab6a0c816d002
bushra <bushra.albadareen@gmail.com>
2021-02-09 21:47:54 +0200

    edit

--- js/app2.js
+++ js/app2.js
@@ -1,44 +1,147 @@
 'use strict';
 
-
+let chance= 3;
 let age = prompt('how older I am?');
 
-let output= '',
+for (let i=0; i < chance; i++){
 
-if (age === 30){
-  output= 'right answer';
+  if (age === 30 ){
+    alert ('right answer');
+  }
+  else if (age < 30) {
+    alert ('wrong answer (too low)');
+    age = prompt('how older I am?');
+  }
+  else if (age > 30) {
+    alert ('wrong answer (too high)');
+    age = prompt('how older I am?');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer 30');
+  break;
 }
-else if (age < 30) {
-  output= 'wrong answer (too low)';
+
+let weight = prompt ('how do i weight?');
+
+
+for (let i=0; i < chance; i++){
+
+  if (weight === 60 ){
+    alert ('right answer');
+  }
+  else if (weight < 60) {
+    alert ('wrong answer (too low)');
+    weight = prompt ('how do i weight?');
+  }
+  else if (weight > 60) {
+    alert ('wrong answer (too high)');
+    weight = prompt ('how do i weight?');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer 60');
+  break;
+
 }
-else if (age > 30) {
-  output= 'wrong answer (too high)';
-}
-
-let attempt = 4;
-result='';
 
 
-for (i=0; i<= attempt; i++){
-  result= age + output;
-}
-
-
-document.write(result)
-
-
-
-
-
-
-
-
-
-
-
-
-let weight = prompt('how much do I weight?');
 let hight = prompt('how much do i hight?');
+
+
+for (let i=0; i < chance; i++){
+
+  if (hight === 153 ){
+    alert ('right answer');
+  }
+  else if (hight < 153) {
+    alert ('wrong answer (too low)');
+    hight = prompt('how much do i hight?');
+  }
+  else if (hight > 153) {
+    alert ('wrong answer (too high)');
+    hight = prompt('how much do i hight?');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer 153');
+  break;
+
+}
 let placeVisit = prompt('how many countries have I traveled to');
+
+for (let i=0; i < chance; i++){
+
+  if (placeVisit === 2 ){
+    alert ('right answer');
+  }
+  else if (placeVisit < 2) {
+    alert ('wrong answer (too low)');
+    placeVisit = prompt('how many countries have I traveled to');
+  }
+  else if (placeVisit > 2) {
+    alert ('wrong answer (too high)');
+    placeVisit = prompt('how many countries have I traveled to');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer 2');
+  break;
+
+}
 let studyYear = prompt('how many years of my study?');
+
+
+for (let i=0; i < chance; i++){
+
+  if (studyYear === 9 ){
+    alert ('right answer');
+  }
+  else if (studyYear < 9) {
+    alert ('wrong answer (too low)');
+    studyYear = prompt('how many years of my study?');
+  }
+  else if (studyYear > 9) {
+    alert ('wrong answer (too high)');
+    studyYear = prompt('how many years of my study?');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer 9');
+  break;
+
+}
 let bookRead = prompt('how many books have i read?');
+
+
+for (let i=0; i < chance; i++){
+
+  if (bookRead === 10 ){
+    alert ('right answer');
+  }
+  else if (bookRead < 10) {
+    alert ('wrong answer (too low)');
+    bookRead = prompt('how many books have i read?');
+  }
+  else if (bookRead > 10) {
+    alert ('wrong answer (too high)');
+    bookRead = prompt('how many books have i read?');
+  }
+ }
+  
+ for (let i=0; i = chance; i++){
+  alert('final chance, the right answer 10');
+  break;
+
+}
+
+
+
+
+
+
